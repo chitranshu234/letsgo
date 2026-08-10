f84881cfc6668279b2a1a35b79713b5a9049e95f
Chitranshu Pandey <167399221+chitranshu234@users.noreply.github.com>
2026-08-10 22:14:31 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (73.47%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    int[] dp;
+
+    public int climbStairs(int n) {
+
+        dp = new int[n + 1];
+
+        return fun(0, n);
+    }
+
+    int fun(int i, int n) {
+
+        if (i == n) {
+            return 1;
+        }
+
+        if (i > n) {
+            return 0;
+        }
+
+        if (dp[i] != 0) {
+            return dp[i];
+        }
+
+        int a1 = fun(i + 1, n);
+
+        int a2 = fun(i + 2, n);
+
+        int ans = a1 + a2;
+
+        dp[i] = ans;
+
+        return ans;
+    }
+}
